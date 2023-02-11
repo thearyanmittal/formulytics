283053bac245e89921ea7fb0b61ae3408c2369b0
Aryan Mittal <thearyanmittal@gmail.com>
2023-02-11 18:11:48 -0500

    updated win probability and send data

--- analysis/send_data.py
+++ analysis/send_data.py
@@ -1,4 +1,17 @@
 import pandas as pd
-import time
+import requests
 
 out = pd.read_excel('data/out.xlsx')
+
+for (index, row) in out.iterrows():
+    name = row['driver_name']
+    lap = row['lap_index']
+    laptime = row['lap_time']
+    pitstoptime = row['pit_stop_time']
+    prevelapsed = row['prev_elapsed']
+    position = row['position']
+    probability = round(1 / position, 2)
+    requests.post(
+        f'http://ec2-3-22-63-209.us-east-2.compute.amazonaws.com:8080/addRacerData?name={name}&lap={lap}&laptime={laptime}&pitstoptime={pitstoptime}&prevelapsed={prevelapsed}&position={position}&probability={probability}'
+    )
+    break
