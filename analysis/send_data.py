5a091bb4845d244317194428ed61564a200070a7
Aryan Mittal <thearyanmittal@gmail.com>
2023-02-12 02:27:45 -0500

    added probabilities to send_data.py

--- analysis/send_data.py
+++ analysis/send_data.py
@@ -10,7 +10,7 @@ for (index, row) in out.iterrows():
     pitstoptime = row['pit_stop_time']
     prevelapsed = row['prev_elapsed']
     position = row['position']
-    probability = round(1 / position, 2) # expected finish position
+    probability = [row['prob_up'], row['prob_down'], row['prob_same'], row['prob_podium'], row['prob_win']]
     requests.post(
         f'http://ec2-3-22-63-209.us-east-2.compute.amazonaws.com:8080/addRacerData?name={name}&lap={lap}&laptime={laptime}&pitstoptime={pitstoptime}&prevelapsed={prevelapsed}&position={position}&probability={probability}'
     )
