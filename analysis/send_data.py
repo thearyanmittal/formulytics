f25e4d09763fc80f9903676101cc5e4d7c226a71
Aryan Mittal <thearyanmittal@gmail.com>
2023-02-12 01:50:41 -0500

    added neural network

--- analysis/send_data.py
+++ analysis/send_data.py
@@ -10,8 +10,7 @@ for (index, row) in out.iterrows():
     pitstoptime = row['pit_stop_time']
     prevelapsed = row['prev_elapsed']
     position = row['position']
-    probability = round(1 / position, 2)
+    probability = round(1 / position, 2) # expected finish position
     requests.post(
         f'http://ec2-3-22-63-209.us-east-2.compute.amazonaws.com:8080/addRacerData?name={name}&lap={lap}&laptime={laptime}&pitstoptime={pitstoptime}&prevelapsed={prevelapsed}&position={position}&probability={probability}'
     )
-    break
